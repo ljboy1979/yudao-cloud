6b34b103fd0d74bfd9199f2832c2b0bed4500c0a
liujun <liujun001@acsm.cn>
2022-11-15 00:27:58 +0800

    为了高度还原本地环境加上：自定义基于相同网段优先的客户端负载均衡

---
+++ yudao-framework/yudao-spring-boot-starter-web/src/main/java/cn/iocoder/yudao/framework/loadbalancer/LoadBalanceConfig.java
@@ -0,0 +1,30 @@
+package cn.iocoder.yudao.framework.web.core.loadbalancer;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.cloud.loadbalancer.support.SimpleObjectProvider;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration(proxyBeanMethods = false)
+public class LoadBalanceConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.cloud.loadbalancer.configurations", havingValue = "net-segment")
+    public ServiceInstanceListSupplier serviceInstanceListSupplier(ConfigurableApplicationContext context) {
+        ServiceInstanceListSupplier supplier = ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().withCaching().build(context);
+        return new NetSegmentServiceInstanceListSupplier(supplier);
+    }
+
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> reactorServiceInstanceLoadBalancer(Environment environment, ServiceInstanceListSupplier serviceInstanceListSupplier) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RoundRobinLoadBalancer(new SimpleObjectProvider<>(serviceInstanceListSupplier), name);
+    }
+}
