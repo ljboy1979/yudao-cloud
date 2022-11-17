d4f7216a434e02241f6398523a9ffb53737e8e20
DESKTOP-AP86TV1\PC003 <gaoht_xx@163.com>
2022-11-17 15:25:32 +0800

    拆分代理商管理--ght

---
+++ yudao-ui-admin/src/views/member/componets/agentManagement.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="title">代理商管理</div>
+        <el-input style="width: 150px;" v-model="input" placeholder="请输入内容"></el-input>
+        <el-button style="margin-left: 15px;" type="primary" icon="el-icon-search" @click="handleQuery">搜索
+        </el-button>
+        <el-button icon="el-icon-refresh" @click="resetQuery">重置</el-button>
+        <el-table v-loading="loading" :data="list" stripe>
+            <el-table-column label="代理商名称" align="center" prop="id" />
+            <el-table-column label="行政区域" align="center" prop="nickname" />
+            <el-table-column label="手机号" align="center" prop="avatar" />
+            <el-table-column label="地址" align="center" prop="status" />
+            <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
+                        v-hasPermi="['member:user:update']">编辑</el-button>
+                    <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
+                        v-hasPermi="['member:user:delete']">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+<script>
+export default {
+    name: 'agentManagement',
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            // 遮罩层
+            // loading: true,
+            loading: false,
+            input: "",
+            list: [],
+
+        }
+    },
+    created() {
+        console.log(this.id)
+    },
+    methods: {
+        handleQuery() {
+
+        },
+        handleUpdate() {
+
+        },
+        handleDelete() {
+
+        },
+        //
+        resetQuery() {
+
+        }
+    }
+}
+</script>
+<style scoped>
+.title {
+    font-size: 24px;
+    margin: 10px 0 20px 15px;
+}
+</style>
--- yudao-ui-admin/src/views/member/user/memberDetail.vue
+++ yudao-ui-admin/src/views/member/user/memberDetail.vue
@@ -83,25 +83,7 @@
                 </el-select>
             </el-tab-pane>
             <el-tab-pane label="代理商管理" name="ninth">
-                <div class="title">代理商管理</div>
-                <el-input style="width: 150px;" v-model="input" placeholder="请输入内容"></el-input>
-                <el-button style="margin-left: 15px;" type="primary" icon="el-icon-search" @click="handleQuery">搜索
-                </el-button>
-                <el-button icon="el-icon-refresh" @click="resetQuery">重置</el-button>
-                <el-table v-loading="loading" :data="list" stripe>
-                    <el-table-column label="代理商名称" align="center" prop="id" />
-                    <el-table-column label="行政区域" align="center" prop="nickname" />
-                    <el-table-column label="手机号" align="center" prop="avatar" />
-                    <el-table-column label="地址" align="center" prop="status" />
-                    <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-                        <template slot-scope="scope">
-                            <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)"
-                                v-hasPermi="['member:user:update']">编辑</el-button>
-                            <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
-                                v-hasPermi="['member:user:delete']">删除</el-button>
-                        </template>
-                    </el-table-column>
-                </el-table>
+                <agentManagement :id="userid" ></agentManagement>
             </el-tab-pane>
             <el-tab-pane label="钱包交易记录" name="tenth">
                 <div class="title">钱包交易记录</div>
@@ -132,10 +114,12 @@
 <script>
 import { createUser, updateUser, deleteUser, getUser, getUserPage, exportUserExcel } from "@/api/member/user";
 import memberPointsRecord  from "../componets/memberPointsRecord.vue"
+import agentManagement  from "../componets/agentManagement.vue"
 export default {
     name: "User",
     components: {
-        memberPointsRecord
+        memberPointsRecord,
+        agentManagement
     },
     data() {
         return {
