2688caf390299c54cea4b77655cc4afb3fc76dd9
DESKTOP-LTL31F4\Z006 <1131795065@qq.com>
2022-11-18 10:25:08 +0800

    修改组件动态加载--luojinbo

--- yudao-ui-admin/src/views/member/user/memberDetail.vue
+++ yudao-ui-admin/src/views/member/user/memberDetail.vue
@@ -62,12 +62,12 @@
                     <el-table-column label="订单状态" align="center" prop="password" />
                 </el-table>
             </el-tab-pane>
-            <el-tab-pane label="进出记录" name="sixth">
+            <el-tab-pane label="进出记录" name="passInAndOutRecord">
                 <div class="title">进出记录</div>
-                <passInAndOutRecord :id="userid" v-if="DynamicLoading[5].Loading"></passInAndOutRecord>
+                <passInAndOutRecord :id="userid" v-if="activeName=='passInAndOutRecord'"></passInAndOutRecord>
             </el-tab-pane>
-            <el-tab-pane label="会员积分记录" name="seventh">
-                <memberPointsRecord :id="userid" v-if="DynamicLoading[6].Loading"></memberPointsRecord>
+            <el-tab-pane label="会员积分记录" name="memberPointsRecord">
+                <memberPointsRecord :id="userid" v-if="activeName=='memberPointsRecord'"></memberPointsRecord>
             </el-tab-pane>
             <el-tab-pane label="健康档案" name="eighth">
                 <div class="title">健康档案</div>
@@ -76,11 +76,11 @@
                     </el-option>
                 </el-select>
             </el-tab-pane>
-            <el-tab-pane label="代理商管理" name="ninth">
-                <agentManagement :id="userid" v-if="DynamicLoading[8].Loading"></agentManagement>
+            <el-tab-pane label="代理商管理" name="agentManagement">
+                <agentManagement :id="userid" v-if="activeName=='agentManagement'"></agentManagement>
             </el-tab-pane>
-            <el-tab-pane label="钱包交易记录" name="tenth">
-                <WalletTransactionHistory :id="userid" v-if="DynamicLoading[9].Loading"></WalletTransactionHistory>
+            <el-tab-pane label="钱包交易记录" name="WalletTransactionHistory">
+                <WalletTransactionHistory :id="userid" v-if="activeName =='WalletTransactionHistory'"></WalletTransactionHistory>
             </el-tab-pane>
         </el-tabs>
     </div>
@@ -183,7 +183,6 @@ export default {
             //新增会员积分记录
             userid: '',
             isOrno: false,
-            DynamicLoading: [{ id: '0', Loading: false }, { id: '1', Loading: false }, { id: '2', Loading: false }, { id: '3', Loading: false }, { id: '4', Loading: false }, { id: '5', Loading: false }, { id: '6', Loading: false }, { id: '7', Loading: false }, { id: '8', Loading: false }, { id: '9', Loading: false }]
         };
     },
     created() {
@@ -195,12 +194,6 @@ export default {
         //切换tab
         handleClick(tab, event) {
             console.log(tab.index, event);
-            // tab.index==6?this.isOrno=true:''
-            if (this.DynamicLoading[tab.index]) {
-                this.DynamicLoading[tab.index].Loading = true
-            }
-
-
         },
         //会员积分记录搜索
         handleQuery() {
