5c3e4c19cb52197333c18354179fc3fb8492b7f7
lihongyan <565284407@qq.com>
2022-12-05 17:15:09 +0800

    1、前端返显创建时间格式问题

--- acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/patient/vo/patienthealth/PatientHealthRespVO.java
+++ acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/patient/vo/patienthealth/PatientHealthRespVO.java
@@ -1,11 +1,13 @@
 package cn.acsm.module.member.user.controller.admin.patient.vo.patienthealth;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-import java.time.LocalDateTime;
-import java.util.*;
-import io.swagger.annotations.*;
+import java.util.Date;
 
 import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 import static cn.iocoder.yudao.framework.common.util.date.DateUtils.TIME_ZONE_DEFAULT;
@@ -20,6 +22,7 @@ public class PatientHealthRespVO extends PatientHealthBaseVO {
     private Long id;
 
     @ApiModelProperty(value = "创建时间", required = true)
-    private LocalDateTime createTime;
+    @JsonFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND, timezone = TIME_ZONE_DEFAULT)
+    private Date createTime;
 
 }
--- acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/patient/vo/prescription/PrescriptionRecordRespVO.java
+++ acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/patient/vo/prescription/PrescriptionRecordRespVO.java
@@ -1,10 +1,16 @@
 package cn.acsm.module.member.user.controller.admin.patient.vo.prescription;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-import java.time.LocalDateTime;
-import java.util.*;
-import io.swagger.annotations.*;
+import java.util.Date;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.TIME_ZONE_DEFAULT;
 
 @ApiModel("管理后台 - 处方记录表 Response VO")
 @Data
@@ -16,6 +22,7 @@ public class PrescriptionRecordRespVO extends PrescriptionRecordBaseVO {
     private Long id;
 
     @ApiModelProperty(value = "创建时间", required = true)
-    private LocalDateTime createTime;
+    @JsonFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND, timezone = TIME_ZONE_DEFAULT)
+    private Date createTime;
 
 }
