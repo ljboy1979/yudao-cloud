a5bdc23e84152c13155a8d373243e7c39676b48a
lihongyan <565284407@qq.com>
2022-12-13 13:43:40 +0800

    1、创建时间和修改时间格式化

--- acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/integral/vo/record/IntegralRecordRespVO.java
+++ acsm-module-member/acsm-module-member-user/acsm-module-member-user-biz/src/main/java/cn/acsm/module/member/user/controller/admin/integral/vo/record/IntegralRecordRespVO.java
@@ -1,8 +1,11 @@
 package cn.acsm.module.member.user.controller.admin.integral.vo.record;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import java.util.*;
 import io.swagger.annotations.*;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
 @ApiModel("管理后台 - 会员积分记录 Response VO")
 @Data
@@ -14,6 +17,7 @@ public class IntegralRecordRespVO extends IntegralRecordBaseVO {
     private Long id;
 
     @ApiModelProperty(value = "创建时间", required = true)
+    @JsonFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private Date createTime;
 
 }
