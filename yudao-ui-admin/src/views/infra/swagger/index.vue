2ddfdd2ddaf60329a462599f526da9f4989371e0
wangdong <wangdong@acsm.cn>
2022-12-19 14:11:22 +0800

    wangd_update_20221219 修改swagger的地址配置。

--- yudao-ui-admin/src/views/infra/swagger/index.vue
+++ yudao-ui-admin/src/views/infra/swagger/index.vue
@@ -12,8 +12,8 @@ export default {
   data() {
     return {
       // url: process.env.VUE_APP_BASE_API + "/doc.html"
-      url: process.env.VUE_DOC + "/doc.html"
-      // url: "http://123.56.19.224:49087" + "/doc.html"
+      // url: process.env.VUE_DOC + "/doc.html"
+      url: "http://123.56.19.224:49087" + "/doc.html"
     };
   },
   created(){
