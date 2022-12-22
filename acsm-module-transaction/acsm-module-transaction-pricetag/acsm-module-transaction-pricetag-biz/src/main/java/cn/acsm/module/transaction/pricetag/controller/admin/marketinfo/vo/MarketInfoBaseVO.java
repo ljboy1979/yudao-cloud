84f39bf697039cfa0074632fa2fb9ea976055c83
changkaixin <changkaixin@acsm.cn>
2022-12-22 14:02:13 +0800

    价格牌管理

--- acsm-module-transaction/acsm-module-transaction-pricetag/acsm-module-transaction-pricetag-biz/src/main/java/cn/acsm/module/transaction/pricetag/controller/admin/marketinfo/vo/MarketInfoBaseVO.java
+++ acsm-module-transaction/acsm-module-transaction-pricetag/acsm-module-transaction-pricetag-biz/src/main/java/cn/acsm/module/transaction/pricetag/controller/admin/marketinfo/vo/MarketInfoBaseVO.java
@@ -50,7 +50,6 @@ public class MarketInfoBaseVO {
     private String source;
 
     @ApiModelProperty(value = "状态", required = true)
-    @NotNull(message = "状态不能为空")
     private Integer status;
 
     @ApiModelProperty(value = "经营主体id")
