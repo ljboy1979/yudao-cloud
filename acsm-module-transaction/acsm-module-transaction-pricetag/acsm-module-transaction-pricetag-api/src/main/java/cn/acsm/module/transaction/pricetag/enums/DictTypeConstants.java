04fc6c706be29d09fa7a9d5b5e2f311b6eb89768
wangdong <wangdong@acsm.cn>
2022-12-22 17:03:48 +0800

    wangd_update_20221222 修改市场管理的创建时间返回格式化。

---
+++ acsm-module-transaction/acsm-module-transaction-pricetag/acsm-module-transaction-pricetag-api/src/main/java/cn/acsm/module/transaction/pricetag/enums/DictTypeConstants.java
@@ -0,0 +1,10 @@
+package cn.acsm.module.transaction.pricetag.enums;
+
+/**
+ * Infra 字典类型的枚举类
+ *
+ * @author wangd
+ */
+public interface DictTypeConstants {
+    String MARKET_SOURCE = "market_source"; // 来源
+}
