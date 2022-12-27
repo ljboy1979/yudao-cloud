957afff93548ba012e1524d3ef950fed405eec67
changkaixin <changkaixin@acsm.cn>
2022-12-27 14:09:19 +0800

    分类出参

--- acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/rawmaterialclassify/vo/RawMaterialClassifyRespVO.java
+++ acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/rawmaterialclassify/vo/RawMaterialClassifyRespVO.java
@@ -1,8 +1,11 @@
 package cn.acsm.module.transaction.sales.controller.admin.rawmaterialclassify.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import java.util.*;
 import io.swagger.annotations.*;
+
+import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
 @ApiModel("管理后台 - 原料分类 Response VO")
 @Data
@@ -14,6 +17,9 @@ public class RawMaterialClassifyRespVO extends RawMaterialClassifyBaseVO {
     private String id;
 
     @ApiModelProperty(value = "创建时间", required = true)
+    @JsonFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private Date createTime;
-
+    @ApiModelProperty(value = "更新时间", required = true)
+    @JsonFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private Date updateTime;
 }
