ee8764385dd797328df190b50911073e46fe28fd
changkaixin <changkaixin@acsm.cn>
2022-12-27 14:38:53 +0800

    分类入参

--- acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/packageclassify/vo/PackageClassifyBaseVO.java
+++ acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/packageclassify/vo/PackageClassifyBaseVO.java
@@ -56,7 +56,7 @@ public class PackageClassifyBaseVO {
     private String sort;
 
     @ApiModelProperty(value = "状态", required = true)
-    @NotNull(message = "状态不能为空")
+    //@NotNull(message = "状态不能为空")
     private Integer status;
 
     @ApiModelProperty(value = "经营主体id")
