eb48fc3927aa9aace27b9d92da9e0da18f4830bb
changkaixin <changkaixin@acsm.cn>
2022-12-28 14:31:15 +0800

    套餐管理

--- acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/packageorganization/vo/PackageOrganizationBaseVO.java
+++ acsm-module-transaction/acsm-module-transaction-sales/acsm-module-transaction-sales-biz/src/main/java/cn/acsm/module/transaction/sales/controller/admin/packageorganization/vo/PackageOrganizationBaseVO.java
@@ -82,7 +82,7 @@ public class PackageOrganizationBaseVO {
     private String imgUrl;
 
     @ApiModelProperty(value = "状态", required = true)
-    @NotNull(message = "状态不能为空")
+    //@NotNull(message = "状态不能为空")
     private Integer status;
 
     @ApiModelProperty(value = "经营主体id")
