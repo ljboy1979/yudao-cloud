37ce32bae945ab01d1389702bd9d619063f0cd69
changkaixin <changkaixin@acsm.cn>
2022-12-28 16:48:35 +0800

    货架规格列表

--- acsm-module-transaction/acsm-module-transaction-shelves/acsm-module-transaction-shelves-biz/src/main/java/cn/acsm/module/transaction/shelves/dal/mysql/specifications/SpecificationsMapper.java
+++ acsm-module-transaction/acsm-module-transaction-shelves/acsm-module-transaction-shelves-biz/src/main/java/cn/acsm/module/transaction/shelves/dal/mysql/specifications/SpecificationsMapper.java
@@ -21,6 +21,7 @@ public interface SpecificationsMapper extends BaseMapperX<SpecificationsDO> {
         return selectPage(reqVO, new LambdaQueryWrapperX<SpecificationsDO>()
                 .likeIfPresent(SpecificationsDO::getSpecificationsName, reqVO.getSpecificationsName())
                 .eqIfPresent(SpecificationsDO::getPackagingType, reqVO.getPackagingType())
+                .eqIfPresent(SpecificationsDO::getShelvesId, reqVO.getShelvesId())
                 .betweenIfPresent(SpecificationsDO::getCreateTime, reqVO.getCreateTime())
                 .orderByDesc(SpecificationsDO::getId));
     }
