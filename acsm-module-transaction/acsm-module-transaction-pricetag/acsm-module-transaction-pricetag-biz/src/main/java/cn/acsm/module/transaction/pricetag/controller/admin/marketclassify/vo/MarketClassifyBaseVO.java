efefb3468b53db158b7a787c9412cfb3a6254e7c
changkaixin <changkaixin@acsm.cn>
2022-12-30 14:32:33 +0800

    长度限制

--- acsm-module-transaction/acsm-module-transaction-pricetag/acsm-module-transaction-pricetag-biz/src/main/java/cn/acsm/module/transaction/pricetag/controller/admin/marketclassify/vo/MarketClassifyBaseVO.java
+++ acsm-module-transaction/acsm-module-transaction-pricetag/acsm-module-transaction-pricetag-biz/src/main/java/cn/acsm/module/transaction/pricetag/controller/admin/marketclassify/vo/MarketClassifyBaseVO.java
@@ -16,6 +16,7 @@ public class MarketClassifyBaseVO {
 
     @ApiModelProperty(value = "分类名称", required = true)
     //@NotNull(message = "分类名称不能为空")
+    @Size(max = 10 , min = 0,message = "分类名称个数必须在0和10之间")
     private String categoryName;
 
     @ApiModelProperty(value = "父级编号", required = true)
